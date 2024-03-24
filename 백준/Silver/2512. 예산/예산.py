8ec83e091134888d92c9b07333e59ed31f34641c
sweetpotato15 <115866228+sweetpotato15@users.noreply.github.com>
2024-03-24 18:39:38 +0900

    [Silver II] Title: 예산, Time: 72 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,28 +1,31 @@
 n = int(input())
-budget = list(map(int, input().split()))
-maximum = int(input())
+budget_list = list(map(int, input().split()))
+budget = int(input())
 
+# ## (def 함수 사용)
 
-# 0, max(budget)을 index 로 잡고 이진탐색 
-# maximum 값과 합친 금액을 비교
+def binary_search(array, start, end, target):
+    maximum = (start + end) // 2
+    remainder = 0
+    global answer
 
-def Budget(array, start, end, target):
-    while start <= end:
-        my_max = (start + end) // 2
+    for i in range(n):
+        remainder += min(maximum, array[i])
 
-        money = 0
-        for i in array:
-            money += min(my_max, i)
+    if start > end:
+        answer = maximum
+        return answer
+    
+    if remainder > budget: # 예산 초과인경우
+        end = maximum - 1
+        binary_search(array, start, end, target)
 
-        # 총 예산이 더 적을 경우에는 상한액 내리기
-        if money > target :
-            end = my_max - 1
-        
-        # 총 예산이 많거나 같을 경우에는 일단 저장 후 상한액 올리기
-        else:
-            answer = my_max
-            start = my_max + 1
-
+    else:
+        answer = maximum
+        start = maximum + 1
+        binary_search(array, start, end, target)
+    
     return answer
 
-print(Budget(budget, 0, max(budget), maximum))
+
+print(binary_search(budget_list, 0, max(budget_list), budget))
