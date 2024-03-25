ce2707013b3f0176e86f57376d2dca889763418a
sweetpotato15 <115866228+sweetpotato15@users.noreply.github.com>
2024-03-25 14:31:11 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,13 @@
+
+while True:
+
+    n,m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+
+    if n % m == 0:
+        print("multiple")
+    elif m % n == 0:
+        print("factor")
+    else:
+        print("neither")
