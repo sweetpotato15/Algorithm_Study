9cc9af6f0d316f40c65d5f2b6316d4e02926f14c
sweetpotato15 <115866228+sweetpotato15@users.noreply.github.com>
2024-03-28 17:12:02 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 680 ms, Memory: 68892 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+
+n,m,start = map(int, input().split())
+graph = [[] for i in range(n+1)]
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for nodes in graph:
+    nodes.sort()
+
+def dfs(graph, visited, v):
+    visited[v] = True
+    answer.append(v)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, visited, i)
+
+visited = [False] * (n+1)
+answer = []
+
+dfs(graph, visited, start)
+
+result = [0] * (n+1)
+
+for i in range(len(answer)):
+    result[answer[i]] = i+1
+
+for i in result[1:]:
+    print(i)
