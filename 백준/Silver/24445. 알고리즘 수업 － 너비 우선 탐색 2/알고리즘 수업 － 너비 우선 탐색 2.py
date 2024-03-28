85a0f97d6c7656c6bdd936eb3e53b5d5adc8deb8
sweetpotato15 <115866228+sweetpotato15@users.noreply.github.com>
2024-03-28 17:25:07 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 592 ms, Memory: 60832 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+n,m,start = map(int, input().split())
+graph =[[] for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for nodes in graph:
+    nodes.sort(reverse=True)
+
+visited = [False] * (n+1)
+answer = []
+
+def bfs(graph, visited, start):
+    queue = deque([start])
+    visited[start] = True
+    answer.append(start)
+
+    while queue:
+        v = queue.popleft()
+        for node in graph[v] :
+            if not visited[node]:
+                visited[node] =True
+                queue.append(node)
+                answer.append(node)
+
+result = [0]*(n+1)
+bfs(graph, visited, start)
+
+for i in range(len(answer)):
+    result[answer[i]] = i+1
+
+for ans in result[1:]:
+    print(ans)
