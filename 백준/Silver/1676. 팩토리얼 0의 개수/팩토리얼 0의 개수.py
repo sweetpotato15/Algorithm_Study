24d873933b8c0a8d2035c0fb5fd1a271379f6d4e
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-04-11 23:08:06 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,14 +1,6 @@
 n = int(input())
 
-n2 = 0
 n5 = 0
-for i in range(1, n+1):
-    for j in range(1,n//2 +1):
-        if i % (2**j) == 0:
-            n2 += 1
-        else:
-            continue
-
 for i in range(1, n+1):
     for j in range(1,n//5 +1):
         if i % (5**j) == 0:
@@ -16,4 +8,4 @@ for i in range(1, n+1):
         else:
             continue
 
-print(min(n2, n5))
+print(n5)
