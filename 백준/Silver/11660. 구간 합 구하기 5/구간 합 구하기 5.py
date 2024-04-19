ddd8436f641c58d0cf9244f71e76a9df9146cef4
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-04-20 00:03:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1020 ms, Memory: 105376 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,21 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n,m = map(int, input().split()) # n : 표 크기, m : 구하는 횟수
+A = [[0]*(n+1) for _ in range(n+1)]
 
-A = [0] * (n+1)
-D = [[0] * (n+1) for _ in range(n+1)]
-
-A[0] = [0] * (n+1)
 for i in range(1,n+1):
     A[i] = [0] + list(map(int, input().split()))
-
-# D(x,y) = (1,1) ~ (x,y) 까지 합 구하기
+S = [[0]*(n+1) for _ in range(n+1)] # S  -> (1,1)부터 (i,j)까지 합
 for i in range(1,n+1):
     for j in range(1,n+1):
-        D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j]
+        S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + A[i][j]
 
-#(x1,y1) ~ (x2,y2) 까지 합 구하기
-for i in range(m):
+for _ in range(m):
     x1,y1,x2,y2 = map(int, input().split())
-    print(D[x2][y2] - D[x2][y1-1] - D[x1-1][y2] + D[x1-1][y1-1])
+    answer = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1]
+    print(answer)
