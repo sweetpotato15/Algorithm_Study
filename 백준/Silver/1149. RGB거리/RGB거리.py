359da37fb2b1e97c90b0357f602af2d730fa109f
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-04-22 22:12:12 +0900

    [Silver I] Title: RGB거리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,32 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-# dp 테이블에 (i,j) 까지 칠할 수 있는 최소비용 넣기 (index, 앞뒤 같은색 주의)
-
-cost = [0] * n
+graph = [[] for _ in range(n)]
 for i in range(n):
-    cost[i] = list(map(int, input().split()))
+    graph[i] = list(map(int, input().split()))
 
-# dp 의 첫 행은 cost 의 첫 행과 동일 
-dp = [0] * n
-dp[0] = cost[0]
-for i in range(1,n):
-    dp[i] = [0]*3
-
-# dp에 채우기 
-for i in range(1,n):
+dp = [graph[0]] + [[0] * 3 for _ in range(n-1)]
+for i in range(1, n):
     for j in range(3):
-        if j == 0:
-            left = 1
-            right = 2
-            
-        elif j == 1:
-            left = -1
-            right = 1
-            
-        else:
-            left = -2
-            right = -1
-            
-        dp[i][j] = min(dp[i-1][j+left], dp[i-1][j+right]) + cost[i][j]
-
-
-print(min(dp[n-1]))
+        if j == 0: left, right = 1,2
+        elif j == 1: left, right = -1,1
+        else: left, right = -2,-1
+        dp[i][j] = min(dp[i-1][j+left], dp[i-1][j+right]) + graph[i][j]
+print(min(dp[-1]))
