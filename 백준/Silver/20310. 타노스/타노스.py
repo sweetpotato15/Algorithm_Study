5e44a17f6a1e75dc8c8925366c0dc8f2043cc133
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-05-03 19:52:02 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -2,15 +2,25 @@ import sys
 input = sys.stdin.readline
 
 word = input()
-
-count0 = word.count('0') // 2
-count1 = (len(word) - count0 * 2) // 2
 word0 = '0'
 word1 = '1'
 
-if len(word) % 4 == 0:
-    count = len(word) // 4
-    new = word0 * count + word1 * count
-else:
-    new = word0 * count0 + word1*count1
+# new에 필요한 0,1개수
+count0 = word.count('0') // 2 
+count1 = (len(word) - count0 * 2) // 2
+count = 0 # 1을 몇개 pass 할건지
+
+new = ''
+for i in range(len(word)):
+    if len(new) == len(word) // 2:
+        break
+    if word[i] == '0' and count0 >0:
+        new += word0
+        count0 -= 1
+    elif count < count1: # 1을 pass 하는 경우우
+        count += 1
+        continue
+    else:
+        new += word1
+
 print(new)
