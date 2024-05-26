c0ba50dddc219bd3b5a3f1c63434c37f120b62dc
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-05-26 20:37:02 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.04 ms
+메모리: 10.2 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 26일 20:21:37
+2024년 05월 26일 20:37:01
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,29 +1,13 @@
-# 자카드 유사도 : 교집합 크기 / 합집합 크기 (둘다 공집합일 경우에는 1)
-
-
+from collections import Counter
 def solution(str1, str2):
-    str1 = str1.strip().lower()
-    str2 = str2.strip().lower()
-    A, B = [], []
-    for i in range(len(str1)-1):
-        if str1[i].isalpha() and str1[i+1].isalpha():
-            A.append(str1[i:i+2])
-    for i in range(len(str2)-1):
-        if str2[i].isalpha() and str2[i+1].isalpha():
-            B.append(str2[i:i+2])
-            
-    A1 = list(set(A))
-    B1 = list(set(B))
+    A = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    B = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+
+    A1 = Counter(A)
+    B1 = Counter(B)
     
-    up = 0
-    for i in A1:
-        if i in B:
-            up += min(A.count(i),B.count(i))
-            
-    down = len(A) + len(B) - up
-
-    if down == 0:
-        return 65536
-    else:
-        return int((up/down)*65536)
-
+    up = sum((A1&B1).values())
+    down = sum((A1|B1).values())
+    
+    if down == 0: return 65536
+    else: return int((up/down)*65536)
