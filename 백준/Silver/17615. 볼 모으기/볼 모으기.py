7a1277a6a64a9c857a54b62c020363f5849fd1a7
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-06-02 15:09:06 +0900

    [Silver I] Title: 볼 모으기, Time: 460 ms, Memory: 34204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.py
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.py
@@ -1,32 +1,53 @@
-
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 n = int(input())
-word = input().strip()
+string = input().strip()
 
-if word.count('B') == 0 or word.count('R') == 0:
-    answer = 0
-else:
-    # 1) 맨 뒤에 있는 색을 옮기는 경우
-    string = word
-    top = word[-1]
-    while string[-1] == top:
-        string = string[:-1]
-    answer1 = string.count(top)
+count = []
+# 1) 빨을 좌측으로
+c = 0
+answer = 0
+for i in range(n-1,-1,-1):
+    if string[i] == 'R':
+        c += 1
+    elif string[i] == 'B' and c > 0 :
+        answer += c
+        c = 0
+count.append(answer)
 
-    # 2) 맨 뒤가 아닌 색을 옮기는 경우
-    answer2 = n - word.count(word[-1])
+# 2) 빨을 우측으로
+c = 0
+answer = 0
+for i in range(n):
+    if string[i] == 'R':
+        c += 1
+    elif string[i] == 'B' and c>0:
+        answer += c
+        c = 0
+count.append(answer)
 
-    # 3) 맨 앞에 있는 색을 옮기는 경우
-    string = word
-    top = word[0]
-    while string[0] == top:
-        string = string[1:]
-    answer3 = string.count(top)
+# 3) 파를 좌측으로 
+c = 0
+answer = 0
+for i in range(n-1,-1,-1):
+    if string[i] == 'B':
+        c += 1
+    elif string[i] == 'R' and c > 0 :
+        answer += c
+        c = 0
+count.append(answer)
 
-    # 4) 맨 앞이 아닌 색을 옮기는 경우
-    answer4 = n - word.count(word[0])
-    
-    answer = min(answer1, answer2, answer3, answer4)
-print(answer)
+# 4) 파를 우측으로 
+c = 0
+answer = 0
+for i in range(n):
+    if string[i] == 'B':
+        c += 1
+    elif string[i] == 'R' and c>0:
+        answer += c
+        c = 0
+count.append(answer)
+
+print(min(count))
