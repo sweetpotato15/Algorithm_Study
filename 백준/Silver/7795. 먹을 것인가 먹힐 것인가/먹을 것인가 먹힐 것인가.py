bf0164a03671501f8bb08b5f4455b8b1adda09f2
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2024-07-03 10:12:23 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 140 ms, Memory: 36712 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -5,9 +5,18 @@ from bisect import bisect_left
 T = int(input())
 for _ in range(T):
     answer = 0
+    start = 0
     N, M = map(int, input().split())
     A = sorted(list(map(int, input().split())))
     B = sorted(list(map(int, input().split())))
-    for a in A:
-        answer += bisect_left(B, a)
+
+    for i in range(N):
+        while start < M:
+            if A[i] <= B[start]:
+                answer += start
+                break
+            else:
+                start += 1
+        if start == M:
+            answer += start
     print(answer)
