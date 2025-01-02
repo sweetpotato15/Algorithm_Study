619713eb63c479cb50fbb4cc8145b7708019bfea
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2025-01-02 21:22:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 43644 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,19 +2,16 @@ from collections import deque
 
 N, K = map(int, input().split())
 INF = int(1e6)
-array = [INF]*(int(1e6))
 visited = [False]*(int(1e6))
-q = deque([[0, N]]) # (시간, 위치)
-array[N] = 0
+q = deque([(0, N)]) # (시간, 위치)
+visited[N] = True
 
 while q:
     sec, pos = q.popleft()
     if pos == K:
         print(sec)
         break
-    if not visited[pos]:
-        visited[pos] = True
-        for x in [pos-1, pos+1, pos*2]:
-            if not visited[x] and 0<=x<=100000:
-                q.append([sec+1, x])
-                array[x] = min(array[x], sec+1)
+    for x in [pos-1, pos+1, pos*2]:
+        if 0<=x<=100000 and not visited[x]:
+            q.append([sec+1, x])
+            visited[x] = True
