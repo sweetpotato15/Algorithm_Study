439b4197fbff2488e4dd70335864b3a70eb900e7
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2025-03-17 10:12:14 +0900

    [Silver IV] Title: 베스트셀러, Time: 60 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -7,7 +7,6 @@ title = defaultdict(int)
 for _ in range(N):
     word = input().strip()
     title[word] += 1
-value = list(title.values())
-max_value = sorted(value)[-1]
+max_value = max(title.values())
 answer = [i for i,j in title.items() if j == max_value]
 print(sorted(answer)[0])
