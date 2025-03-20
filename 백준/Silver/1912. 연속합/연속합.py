b74182165aae0fdd9f793513b0c7f38c3a106e11
Kim Yookyung <115866228+sweetpotato15@users.noreply.github.com>
2025-03-20 14:09:50 +0900

    [Silver II] Title: 연속합, Time: 76 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -4,16 +4,10 @@ input = sys.stdin.readline
 N = int(input())
 array = list(map(int, input().split()))
 
-smallest = [0]*N
-smallest[0] = array[0]
+dp = [0]*N # dp[i] : i까지 연속 최대 합
+dp[0] = array[0]
 
-# 연속합
-sum_array = array[:]
-for i in range(1,N):
-    sum_array[i] += sum_array[i-1]
-    smallest[i] = min(smallest[i-1], sum_array[i-1])
+for i in range(1, N):
+    dp[i] = max(array[i]+dp[i-1], array[i])
 
-max_value = max(array)
-for i in range(N-1, 0, -1):
-    max_value = max(max_value, sum_array[i] - smallest[i], sum_array[i])
-print(max_value)
+print(max(dp))
